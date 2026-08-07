6c69b0fb71d762f9a93437d6b8e4eb95f0771bd1
unknown <arminjafri138386@gmail.com>
2026-08-07 09:48:18 -0700

    change domain

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://cryptoocapitalhub.com/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="http://155.117.6.87/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
