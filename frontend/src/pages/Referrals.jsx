1210b9e9b82ce06d3744f9f175a33a96d088aa46
unknown <arminjafri138386@gmail.com>
2026-08-11 06:24:42 -0700

    aaa

--- frontend/src/pages/Referrals.jsx
+++ frontend/src/pages/Referrals.jsx
@@ -13,7 +13,7 @@ import {
 // ==========================================
 
 const USER_DATA_KEY = "my_app_user_data";
-const BOT_USERNAME = "aipolynetbot";
+const BOT_USERNAME = "@pooooooooooobot";
 
 // ==========================================
 // LocalStorage Helpers
